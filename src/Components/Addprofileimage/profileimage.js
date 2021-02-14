5032c91f4171b4d00e00503e4b231f34758a831f
Quadrizz <mackquadrizz@gmail.com>
2021-02-14 13:55:57 +0100

    redesigining

--- src/Components/Addprofileimage/profileimage.js
+++ src/Components/Addprofileimage/profileimage.js
@@ -1,16 +1,22 @@
 import React , {useState} from 'react'
 import { useHistory } from "react-router-dom";
-import './profileimage.css'
+import './profileimage.css';
+import Dropzone from 'react-dropzone'
 
 const ProfileImage = ({id, props})=>{
     const history = useHistory()
     // const [loading, setLoading] = useState(false)
     const [image, setImage] = useState("")
+    const [isImage, setIsImage] = useState(false) 
     
-    const addImage = (e)=>{
-        const files = e.target.files
-        setImage(files[0])
-        console.log(image)
+    // const addImage = (e)=>{
+    //     const files = e.target.files
+    //     setImage(files[0])
+    //     console.log(image)
+    // }
+
+    const skip = ()=>{
+        history.push(`/dashboard/${id}`)
     }
 
     const uploadImage = ()=>{
@@ -54,15 +60,37 @@ const ProfileImage = ({id, props})=>{
         })
     }
 
-
     return(
-        <div>
-            <h1>Add profile image</h1>
-            <input type = 'file' name = 'file' placeholder = 'Upload an image'
-             onChange = {addImage} />
-            <button onClick = {uploadImage}>Set Image</button>
+        <div className = 'main_upload'>
+            <h1>Add a profile image</h1>
+            <Dropzone onDrop={acceptedFiles =>{ 
+                setIsImage(true)
+                setImage(acceptedFiles[0])}}>
+                {({getRootProps, getInputProps}) => (
+                <section>
+                    <div {...getRootProps()}>
+                        <input {...getInputProps()} />
+                        <p>{isImage ? "Upload this image" : "Drag 'n' drop Image"}</p>
+                    </div>
+                </section>
+                )}
+            </Dropzone>
+            <div className = 'buttons'>
+                <button onClick = {uploadImage}>Upload Image</button>
+                <button id = 'skip' onClick = {skip}>Skip</button>
+            </div>
         </div>
     )
+
+
+    // return(
+    //     <div>
+    //         <h1>Add profile image</h1>
+    //         <input type = 'file' name = 'file' placeholder = 'Upload an image'
+    //          onChange = {addImage} />
+    //         <button onClick = {uploadImage}>Set Image</button>
+    //     </div>
+    // )
 }
 
 export default ProfileImage;
