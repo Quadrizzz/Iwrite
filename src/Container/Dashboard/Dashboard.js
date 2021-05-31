3f1b258355ea4eb9c0f3b260a9e4b7f17d59607c
Quadrizz <mackquadrizz@gmail.com>
2021-05-31 21:36:08 +0100

    new

--- src/Container/Dashboard/Dashboard.js
+++ src/Container/Dashboard/Dashboard.js
@@ -1,18 +1,30 @@
 import React, {useState, useEffect} from 'react';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import Store from 'store';
 import './Dashboard.css'
 import book from './book.png'
 import dash from './dashboard.png'
 import settings from './settings.png'
-import down from './down.png'
+// import down from './down.png'
 import download from './Downloads.png'
 import upload from './Uploads.png'
+// import { background } from '@chakra-ui/styled-system';
+// import { border } from '@chakra-ui/styled-system';
+// import { Icon } from '@material-ui/core';
 
 
 const Dashboard = ({id, set_id , props})=>{
     const [userdata, setdata] = useState('',{})
     // const [backup_id, set_backup] = useState(id, '')
 
+    const TextStyle = {
+        color: "#fff",
+    };
+
+    const IconSyle = {
+        marginTop: "5px",
+    }
 
     useEffect(() => {
         let mounted = true
@@ -50,7 +62,7 @@ const Dashboard = ({id, set_id , props})=>{
             mounted = false
         }
     })
-
+ 
 
     // useEffect( ()=>{
     //     fetchItems();
@@ -59,45 +71,63 @@ const Dashboard = ({id, set_id , props})=>{
 
     return (
         <div className = "main">
-            <div className = "Navigation">
-                <div className = "Top_nav">
-                    <div>
-                        <button><span><img src = {dash} alt = "work"></img></span>Dashboard</button>
-                    </div>
-                    <div className = "Sub_Top_nav">
-                        <div className = "Sub_Top_nav_div">
-                            <button><span><img src = {settings} alt = "wprk"></img></span>Settings</button>
-                            <button><span><img src = {book} alt = "work"></img></span>Library</button>
+           <div className = "main_view">
+                <SideNav
+                    onSelect={(selected) => {
+                // Add your code here
+                    }}
+                   style ={{background: '#63ace7', border : 'none'}} >
+                    <SideNav.Toggle/>
+                    <SideNav.Nav defaultSelected="" className = "NavBar">
+                        <NavItem eventKey="dashboard" className ="NavItem">
+                            <NavIcon style = {IconSyle}>
+                                <img src = {dash} alt = "dashboard"/>
+                            </NavIcon>
+                            <NavText style ={TextStyle}>
+                                Dashboard
+                            </NavText>
+                        </NavItem>
+                        <NavItem eventKey="Library">
+                            <NavIcon style = {IconSyle}>
+                                <img src ={book} alt = "book"/>
+                            </NavIcon>
+                            <NavText style = {TextStyle}>
+                                Library
+                            </NavText>
+                        </NavItem>
+                        <NavItem eventKey="Settings">
+                            <NavIcon style = {IconSyle}>
+                                <img src = {settings} alt = "settings"/>
+                            </NavIcon>
+                            <NavText style = {TextStyle}>
+                                Settings
+                            </NavText>
+                        </NavItem>
+                    </SideNav.Nav>
+                </SideNav>
+                <div className = "main_container">
+                    <div className = "stat_container">
+                        <h1 id = "hello">Hello, {userdata.name}</h1>
+                        <div className = "stats">
+                            <div className = "downloads">
+                                <img src = {download} alt = "download"/>
+                                <h1>{userdata.downloads}</h1>
+                                <h1>Books Downloaded</h1>
+                            </div>
+                            <div className = "uploads">
+                                <img src = {upload} alt = "upload"/>
+                                <h1>{userdata.uploads}</h1>
+                                <h1>Books Uploaded</h1>
+                            </div>
                         </div>
-                        <div className = "profile">
-                            <button><span><img src = {down} alt = "work"></img></span></button>
-                            <img className = "profile_picture" src = "https://image.shutterstock.com/image-photo/living-coral-color-year-2019-260nw-1250940526.jpg" alt = "work" ></img>
+
+                        <div className = "profileContainer">
+                            
                         </div>
+
                     </div>
                 </div>
-            </div>
-            <div className = "main_section">
-                <h1 className = "welcome_text">Welcome, {userdata.name}</h1>
-                <div className = "main_section_records">
-                    <div className = "upload_download">
-                        <div className = "download">
-                                <h1>Downloads</h1>
-                                <img src = {download} alt = "work"></img>
-                                <h2>{userdata.downloads}</h2>
-                                <button>Download</button>
-                        </div>
-                        <div className = "uploads">
-                                <h1>Uploads</h1>
-                                <img src = {upload} alt = "work"></img>
-                                <h2>{userdata.uploads}</h2>
-                                <button>Upload</button>
-                        </div>
-                    </div>
-                    <div className = "profile_container">
-                        
-                    </div>
-                </div>
-            </div>
+           </div>
         </div>
     )
 }
